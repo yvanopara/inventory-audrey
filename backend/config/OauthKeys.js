f8b620c2af3ecf595a651433ded391f642786fa6
yvanopara <136276693+yvanopara@users.noreply.github.com>
2025-12-18 20:22:13 +0100

    Update OauthKeys.js

--- backend/config/OauthKeys.js
+++ backend/config/OauthKeys.js
@@ -1,7 +1,7 @@
 //addmin Keys
-export const adminEmail = 'audreyleumaleu1@.com';
+export const adminEmail = 'audreyleumaleu1@gmail.com';
 export const adminPassword = '00000000';
 
 //UserKeys
-export const userEmail = 'audreyleumaleu1@.com';
+export const userEmail = 'audreyleumaleu1@gmail.com';
 export const userPassword = '00000000';
